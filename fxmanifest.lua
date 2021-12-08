0d135b8c2d5cac3432427aeebe8688366d6836b9
Mate <35634379+Matetv@users.noreply.github.com>
2021-12-08 09:48:01 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,6 @@ ui_page 'html/index.html'
 
 shared_scripts {
     'config.lua',
-	'@qb-core/import.lua',
     '@qb-apartments/config.lua',
     '@qb-garages/config.lua',
 }
@@ -33,11 +32,6 @@ files {
 }
 exports {
     'InPhone',
-	 'SetPlayerAlive',
-    'EstablishCall',
-     'EndCall',
- 'SetPlayerRadioSpeaker',
- 'SetPlayerRadioChannel',
- 'RemovePlayerRadioChannel',
- 'SetRadioTowers',
+    'EstablishCall'
 }
+lua54 'yes'
